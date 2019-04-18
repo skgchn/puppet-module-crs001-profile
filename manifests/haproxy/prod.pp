09517bc12f8a304578098927b2a91954554962bc
Sunil Girdhar <sunil.g@poweredbyu.com>
2019-04-18 14:01:33 +0530

    first commit

---
+++ manifests/haproxy/prod.pp
@@ -0,0 +1,22 @@
+class profile::haproxy::prod {
+  include ::haproxy
+  haproxy::listen { 'lb-01':
+    collect_exported => false,
+    ipaddress        => $::ipaddress,
+    ports            => '80',
+  }
+  haproxy::balancermember { 'node1':
+    listening_service => 'lb-01',
+    server_names      => 'node1',
+    ipaddresses       => '223.23.2.22',
+    ports             => '8080',
+    options           => 'check',
+  }
+  haproxy::balancermember { 'node2':
+    listening_service => 'lb-01',
+    server_names      => 'node2',
+    ipaddresses       => '223.23.2.23',
+    ports             => '8080',
+    options           => 'check',
+  }
+}
---
+++ manifests/tomcat/prod.pp
@@ -0,0 +1,12 @@
+class profile::tomcat::prod {
+  include tomcat # Since using hieradata, cannot use class
+  #class {'::tomcat' :
+  #  java_opts_xms => '128m',
+  #  java_opts_xmx => '256m',
+  #}
+
+  tomcat::deploy {'sysfoo':
+    deploy_url     => 'https://9-180957468-gh.circle-artifacts.com/0/home/circleci/repo/target/sysfoo.war',
+    checksum_value => '3b3ba1ee6a6a30960303054f9f9b9c42',
+  }
+}
